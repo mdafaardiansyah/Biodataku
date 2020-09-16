314e972f7b0d3571ef62c3dc6cf93697cafeba41
mdafaardiansyah <ardidafa21@gmail.com>
2020-09-16 11:36:20 +0700

    Biodataku

---
+++ Biodataku.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+	printf("Nama\t\t\t\t: Muhammad Dafa Ardiansyah. \n");
+	printf("NPM\t\t\t\t: 19081010094 \n");
+	printf("Jenis Kelamin\t\t\t: Laki-laki. \n");
+	printf("Kewarganegaraan\t\t\t: Indonesia. \n");
+	printf("Tempat, Tanggal Lahir\t\t: Sidoarjo, 4 Juni 2002	\n");
+	printf("Tempat Tinggal\t\t\t: Sidoarjo. \n");
+	printf("Pendidikan Saat ini\t\t: Mahasiswa. \n");
+	printf("Pendidikan Terakhir\t\t: SMA. \n");
+	
+	return 0;
+}
